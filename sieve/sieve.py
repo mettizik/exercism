ec50112ab3592a535a3c59d45f6194ac7be3eeca
Mettizik <mokych.apriorit@gmail.com>
2017-04-05 01:28:36 +0300

    Solved exercise

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -1,2 +1,15 @@
-def sieve():
-    pass
+def is_prime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+
+    return True
+
+def sieve(limit):
+    if limit < 2:
+        return []
+    primes = [i for i in range(2, limit) if is_prime(i)]
+    if is_prime(limit):        
+        primes.append(limit)
+
+    return primes
