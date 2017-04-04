4a8fd01eb509ee62dac0c25fd739cdde61bb8212
Mettizik <mokych.apriorit@gmail.com>
2017-04-05 01:39:18 +0300

    Implemented base ciphering logic

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,6 +1,26 @@
-def encode():
-    pass
+import string
 
+direct = string.ascii_lowercase
+backw = string.ascii_lowercase[-1:0:-1] + string.ascii_lowercase[0]
 
-def decode():
-    pass
+def encode(data):
+    data = data.lower()
+    encoded = ''
+    for ch in data:
+        if ch.isalpha():
+            encoded += backw[direct.index(ch)]
+        elif ch.isnumeric():
+            encoded += ch
+    
+    return encoded
+
+def decode(data):
+    data = data.lower()
+    decoded = ''
+    for ch in data:
+        if ch.isalpha():
+            decoded += direct[backw.index(ch)]
+        elif ch.isnumeric():
+            decoded += ch
+    
+    return decoded
