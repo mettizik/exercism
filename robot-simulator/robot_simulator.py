85b825cc67f6a6f6e0fc8ad82f327ea0d984bd82
Mettizik <mokych.apriorit@gmail.com>
2017-04-05 00:48:23 +0300

    stubbed interface

--- robot-simulator/robot_simulator.py
+++ robot-simulator/robot_simulator.py
@@ -1,3 +1,29 @@
+NORTH = 0
+EAST  = 90
+SOUTH = 180
+WEST  = 270
+
 class Robot(object):
-    def __init__(self):
+    def __init__(self, bearing=NORTH, x=0, y=0):
+        self.bearing = bearing
+        self.x = x
+        self.y = y
+        self.coordinates = (self.x, self.y)
+        pass
+
+    def update_bearing(self, delta):
+        self.bearing += delta 
+        self.bearing = self.bearing if self.bearing < 360 else self.bearing - 360
+        self.bearing = self.bearing if self.bearing >= 0 else self.bearing + 360
+
+    def turn_right(self):
+        self.update_bearing(90)
+
+    def turn_left(self):
+        self.update_bearing(-90)
+
+    def advance(self):
+        pass
+
+    def simulate(self, route):
         pass
