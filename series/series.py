74027fa4c36a4f66a6e58bcf4d358135beff2605
Mettizik <mokych.apriorit@gmail.com>
2017-04-05 00:35:13 +0300

    solved series

--- series/series.py
+++ series/series.py
@@ -1,2 +1,14 @@
-def slices():
-    pass
+def slices(data, slice_size):
+    if slice_size <= 0:
+        raise ValueError('Invalid slice size passed {}'.format(slice_size))
+    
+    if slice_size > len(data):
+        raise ValueError('Slice size ({}) is bigger than sequence length ({})'.format(slice_size, len(data)))
+    
+    offset = 0
+    retvals = []
+    while offset + slice_size <= len(data):
+        retvals.append([int(c) for c in data[offset:offset + slice_size]])
+        offset += 1
+
+    return retvals
