90d0820dc59dc591af6b5e02fea08ee5a709645b
Mettizik <mokych.apriorit@gmail.com>
2017-04-05 01:07:15 +0300

    Solved exercise

--- robot-simulator/robot_simulator.py
+++ robot-simulator/robot_simulator.py
@@ -1,7 +1,9 @@
+import math
+
 NORTH = 0
-EAST  = 90
-SOUTH = 180
-WEST  = 270
+EAST  = 0.5 * math.pi
+SOUTH = math.pi
+WEST  = 1.5 * math.pi
 
 class Robot(object):
     def __init__(self, bearing=NORTH, x=0, y=0):
@@ -13,17 +15,27 @@ class Robot(object):
 
     def update_bearing(self, delta):
         self.bearing += delta 
-        self.bearing = self.bearing if self.bearing < 360 else self.bearing - 360
-        self.bearing = self.bearing if self.bearing >= 0 else self.bearing + 360
+        if self.bearing >= 2 * math.pi:
+            self.bearing = self.bearing - 2 * math.pi
+        elif self.bearing < 0:
+            self.bearing = self.bearing + 2 * math.pi
 
     def turn_right(self):
-        self.update_bearing(90)
+        self.update_bearing(0.5 * math.pi)
 
     def turn_left(self):
-        self.update_bearing(-90)
+        self.update_bearing(-0.5 * math.pi)
 
     def advance(self):
-        pass
+        self.x += int(math.sin(self.bearing))
+        self.y += int(math.cos(self.bearing))
+        self.coordinates = (self.x, self.y)
 
     def simulate(self, route):
-        pass
+        for act in route:
+            if act == 'A':
+                self.advance()
+            elif act == 'R':
+                self.turn_right()
+            elif act == 'L':
+                self.turn_left()
