d15abfe406a62084841021e99224cb0719f74ea0
Mettizik <mokych.apriorit@gmail.com>
2017-04-05 21:32:06 +0300

    solved eercise

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,6 @@
-def sum_of_multiples():
-    pass
+def sum_of_multiples(limit: int, bases: list):
+    multiples = []
+    for i in bases:
+        multiples += [ numb for numb in range(limit) if numb % i == 0 ]
+
+    return sum(set(multiples))
