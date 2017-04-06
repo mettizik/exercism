d0776534e0c0689c88a4da241b1e21054d8e1d82
Mettizik <mokych.apriorit@gmail.com>
2017-04-07 01:27:12 +0300

    Solved exercise

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,12 +1,21 @@
 class School(object):
     def __init__(self, schoolname):
         self.name = schoolname
+        self.grades = {}
 
     def add(self, student, grade):
-        pass
+        if grade in self.grades:
+            self.grades[grade].append(student)
+        else:
+            self.grades[grade] = [student]
 
     def sort(self):
-        return []
+        return tuple([
+            (grade, self.grade(grade)) for grade, students in self.grades.items()
+        ])
 
     def grade(self, grade):
-        return []
+        if grade not in self.grades:
+            return []
+        
+        return tuple(sorted(self.grades[grade]))
