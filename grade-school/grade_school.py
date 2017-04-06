0dc1b8bebd11d0b55aae17b5824bd353fb453b6f
Mettizik <mokych.apriorit@gmail.com>
2017-04-07 01:20:53 +0300

    Implemented interface

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,3 +1,12 @@
 class School(object):
-    def __init__(self):
+    def __init__(self, schoolname):
+        self.name = schoolname
+
+    def add(self, student, grade):
         pass
+
+    def sort(self):
+        return []
+
+    def grade(self, grade):
+        return []
