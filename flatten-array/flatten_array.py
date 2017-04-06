26c1b16613ed391e481bc4a1334cc5c40515e4b2
Mettizik <mokych.apriorit@gmail.com>
2017-04-07 02:22:48 +0300

    Solved excercise

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -1,15 +1,9 @@
-def filter_nulls(func):
-    def wrapper(inputs):
-        return func(list(filter(lambda a: not a is None, inputs)))
-    return wrapper
-
-@filter_nulls
 def flatten(inputs):
     retvals = []
-    for i in inputs:
+    for i in list(filter(lambda a: not a is None, inputs)):
         if isinstance(i, list):
             retvals += flatten(i)
-        else:
+        elif not (isinstance(i, tuple) and not i):
             retvals.append(i)
 
-    return sorted(list(set(retvals)))
+    return retvals
