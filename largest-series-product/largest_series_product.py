90d8b19dffcf727a849a398b7d5cf310c48c09a9
Mettizik <mokych.apriorit@gmail.com>
2017-04-06 23:53:20 +0300

    Solved exercise

--- largest-series-product/largest_series_product.py
+++ largest-series-product/largest_series_product.py
@@ -1,2 +1,30 @@
-def largest_product():
-    pass
+import re
+from operator import mul as multiply
+from functools import reduce as accumulate
+
+def validate_product(func):
+    def wrapper(numberstr, maxlen):
+        if not re.match('^\d+$', numberstr):
+            raise ValueError('{} is not a valid number'.format(numberstr))
+        return func(numberstr, maxlen)
+    return wrapper
+
+def validate_maxlen(func):
+    def wrapper(numberstr, maxlen):
+        if maxlen > len(numberstr) or maxlen < 0:
+            raise ValueError('Invalid maxlen  specified')
+        if maxlen == 0:        
+            return 1
+        return func(numberstr, maxlen)
+    return wrapper
+
+@validate_maxlen
+@validate_product
+def largest_product(numberstr, maxlen):
+    vals = []
+    for chunk in [ 
+        numberstr[i:i + maxlen] for i in range(len(numberstr) - maxlen + 1) 
+        ]:
+        vals.append(accumulate(multiply, [int(n) for n in chunk]))
+        
+    return max(vals)
